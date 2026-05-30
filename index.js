e379d0dc95ae4bb45a41c4dd546e1a4c09c48fc0
shadowbedliss <bedlek.janko@gmail.com>
2026-05-30 16:07:28 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,3 +1,12 @@
+console.log('__dirname:', __dirname);
+console.log('DATA_FILE:', DATA_FILE);
+console.log('exists:', fs.existsSync(DATA_FILE));
+
+try {
+    console.log('files:', fs.readdirSync(__dirname));
+} catch (e) {
+    console.error(e);
+}
 // ARCUS: Operations Management Bot
 require('dotenv').config();
 const {
